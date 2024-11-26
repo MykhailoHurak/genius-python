2abfd0eb22ac9e13f38088978e48ef9e9e6bcac7
Mykhailo Hurak <mykhailohurak@gmail.com>
2024-11-26 16:27:36 +0200

    lesson_1_09 task_04

--- lesson_1_09/example_1_09_04.py
+++ lesson_1_09/example_1_09_04.py
@@ -1,12 +1,13 @@
-class Car():
+class Car:
     def __init__(self, type) -> None:
         self.type = type
         self.properties = {}
 
     def set_properties(self, color, cost):
-        self.properties = {"Color": {color}, "Cost": {cost}}
+        self.properties = {"Color": color, "Cost": cost}
 
     def get_properties(self):
+        print(self.properties)
         return self.properties
 
 class PetrolCar(Car):
@@ -14,16 +15,37 @@ class PetrolCar(Car):
         self.type = type
         self.properties = {}
 
-car_01 = PetrolCar("Toyota")
+car_01 = Car("Toyota")
 car_01.set_properties("Red", 12000)
-print(car_01.__dict__)
+car_01.get_properties()
 
 car_02 = PetrolCar("BMW")
 car_02.set_properties("Red", 21000)
-print(car_02.__dict__)
 
 car_03 = PetrolCar("Toyota")
 car_03.set_properties("Black", 16000)
-print(car_03.__dict__)
 
 cars = [car_01, car_02, car_03]
+
+# print(f"Car 01 - {car_01.type}")
+# print(f"Car 02 - {car_02.type}")
+# print(f"Car 03 - {car_03.type}, color: {car_03.color}")
+
+def get_concret_type_car(type):
+    count = 0
+    for car in cars:
+        if car.type == type:
+            count += 1
+
+    print(f"Count of {type} cars: {count}")
+
+def get_concret_color_car(color):
+    count = 0
+    for car in cars:
+        if car.properties["Color"] == color:
+            count += 1
+
+    print(f"Count of {color} cars: {count}")
+
+get_concret_type_car("Toyota")
+get_concret_color_car("Black")
